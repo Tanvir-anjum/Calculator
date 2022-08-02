867ad3dda95081d937ee825461c27b46312b40b9
Tanvir-anjum <80684462+Tanvir-anjum@users.noreply.github.com>
2022-08-02 20:28:55 +0600

    Add files via upload
    Calculator full project

---
+++ script.js
@@ -0,0 +1,26 @@
+let screen = document.getElementById('screen');
+AllButtons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of AllButtons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == '*') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
